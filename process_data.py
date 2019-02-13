a8153472104c6b5904b853bfe45cee7eb735a6d8
Janet Chen <janetchen@Janets-MacBook-Air.local>
2019-02-13 13:09:22 -0500

    Added docstrings

--- process_data.py
+++ process_data.py
@@ -1,9 +1,34 @@
 def read_data():
+    """Read data from file
+
+    This function reads in data from a text file containing patient information
+
+    Args:
+        none
+
+    Returns:
+        in_file.readlines() (list): the test data text file, parsed in line by
+        line
+    """
     in_file = open("test_data.txt", "r")
     return in_file.readlines()
 
 
 def proc_data(raw_data):
+    """Processes data
+
+    This function cleans up the raw input from the data file, stripping all
+    "\n", and then fills the name, age and gender key-value pairs for each
+    patient's dictionary. It then returns the cleaned data and the list of
+    patient dictionaries
+
+    Args:
+        raw_data (list): data as parsed in from the test data text file
+
+    Returns:
+        data (list): list of all patient data with "\n" removed
+        patients (list): list containing dictionaries with partial patient info
+    """
     data = []
     # 'patients' will be a list of dictionaries (one per patient)
     patients = []
@@ -30,6 +55,20 @@ def proc_data(raw_data):
 
 
 def get_tsh(data):
+    """Gets TSH results in float format
+
+    The TSH results from the test file, when read in, appear as one string
+    per patient. This function takes the string and splits it up by comma.
+    It then converts those strings into floats and places those floats back
+    into a list (one per patient).
+
+    Args:
+        data (list): patient data (all)
+
+    Returns:
+        tsh_results (list): list of lists of patient TSH results
+
+    """
     tsh_results = []
     tsh_results_str = []
     j = 0
@@ -46,6 +85,20 @@ def get_tsh(data):
 
 
 def diag(input):
+    """Patient diagnosis from TSH results
+
+    This function takes a list of TSH results for one patient and checks if
+    any result is above 4.0 (in which case it diagnoses "hyperthyroidism"),
+    below 1.0 (in which case it diagnoses "hypothyroidism"), or between 1.0
+    and 4.0, inclusive (diagnoses "normal thyroid function").
+
+    Args:
+        input (list): list of TSH results
+
+    Returns:
+        diagnosis (string): string containing diagnosis based on TSH results
+        (one of the three options above)
+    """
     # initially assume normal thyroid function
     ans = 0
     # check for hyper-/hypothyroidism
@@ -65,14 +118,38 @@ def diag(input):
 
 
 def finish_dict(tsh_results, patients):
+    """Finishes up each patient's dictionary
+
+    So far, all the key-value pairs for each patient's dictionary except for
+    'Diagnosis' and 'TSH' have been filled. This function completes the job.
+
+    Args:
+        tsh_results (list): list of lists of patient TSH results
+        patients (list): partially filled list of patient dictionaries
+
+    Returns:
+        patients (list): complete list of patient dictionaries
+    """
     for i in range(len(patients)):
         patients[i]['Diagnosis'] = diag(tsh_results[i])
         tsh_results[i].sort()
-        patients[i]['TSH results'] = tsh_results[i]
+        patients[i]['TSH'] = tsh_results[i]
     return patients
 
 
 def save_result(i):
+    """Saves patient dictionary to JSON file
+
+    Will be called once for each patient in text file. This function saves
+    each patient's dictionary to an accordingly named JSON file
+    (FirstName-LastName.json)
+
+    Args:
+        i (int): patient index in text file
+
+    Returns:
+        none
+    """
     import json
     filename = patients[i]['First Name'] + "-"
     filename += patients[i]['Last Name'] + ".json"
--- test_process_data.py
+++ test_process_data.py
@@ -7,6 +7,18 @@ import pytest
                           ([2.0, 3.2, 4.1, 3.9], "hyperthyroidism"),
                           ([0.9, 0.8, 3.6], "hypothyroidism")])
 def test_correct_diag(inputs, exp):
+    """Tests diagnosis function
+
+    Tests the 'diag' function in process_data.py, checking if the actual
+    output corresponds to the expected output
+
+    Args:
+        inputs (list): made up TSH results for one patient
+        exp (string): expected diagnosis for that input
+
+    Returns:
+        none
+    """
     ans = diag(inputs)
     print(ans)
     assert ans == exp
